863e48c935a5798da973cc781a39a56720cd0f6f
ibrahimfaheem <i.faheem@hotmail.com>
2014-07-10 12:26:17 +0200

    bla bla

--- novelari/config/routes.rb
+++ novelari/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home_page/home'
+
+  get 'home_page/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
